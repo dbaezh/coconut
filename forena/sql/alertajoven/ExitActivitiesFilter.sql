441327477ec9f23d8ba515ad5c1fe6c13796a9ab
Claudia Nunez Robinson <nunezro2@illinois.edu>
2015-08-11 13:01:44 -0400

    Adding filter por ExitActivities or ClosingActivities

---
+++ forena/sql/alertajoven/ExitActivitiesFilter.sql
@@ -0,0 +1,13 @@
+--ACCESS=access content
+SELECT 'all' AS 'code', 'Todos' AS 'name' 
+UNION ALL 
+SELECT 'Obtienen documentación - ', 'Jóvenes que obtienen documentación'
+UNION ALL 
+SELECT 'Obtienen empleo o pasantía pagada - ', 'Jóvenes que obtienen empleo o pasantía pagada'
+UNION ALL 
+SELECT 'Terminan capacitación técnica - ', 'Jóvenes que terminan capacitación técnica'
+UNION ALL 
+SELECT 'Reinsertados en la escuela - ', 'Jóvenes reinsertados en la escuela'
+UNION ALL 
+SELECT 'Graduados de EPC - ', 'Jóvenes graduados de EPC'
+
