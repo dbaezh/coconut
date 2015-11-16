2087000d895e66148baccab200f8b3816e8e8410
Claudia <claudia.nunez.robinson@gmail.com>
2015-11-15 23:23:18 -0800

    Agregando nuevo reporte para el Indicador20. Correccion del reporte sobre MARP

---
+++ forena/sql/alertajoven/Indicator20.sql
@@ -0,0 +1,19 @@
+-- ACCESS=access content
+select provider_id as provider_id, 
+	field_agency_name_value as provider_name,  
+	count(distinct uuid) as totalUUID
+from (
+select 
+	distinct marp.provider_id, 
+	field_agency_name_value,  
+	marp.uuid 
+from bitnami_drupal7.aj_marp  marp 
+	join bitnami_drupal7.`aj_attendance` atten on atten.uuid = marp.uuid
+	join bitnami_drupal7.`field_data_field_agency_name` agencyName on marp.provider_id = agencyName.entity_id
+	join bitnami_drupal7.`field_data_field_agency_active` agencyActive on marp.provider_id = agencyActive.entity_id
+where
+	field_agency_active_value = 1
+	and marp.provider_id in (:provider_id)
+order by field_agency_name_value
+) distinctMarp
+group by provider_id
--- forena/sql/alertajoven/MARP.sql
+++ forena/sql/alertajoven/MARP.sql
@@ -1,90 +1,79 @@
+--ACCESS=access content
 SELECT 
 'HSH',
-SUM(case when reg.Sexo = 'M' then 1 else 0 end) as 'Male', 
-SUM(case when reg.Sexo = 'F' then 1 else 0 end) as 'Female',
-SUM(case when reg.Sexo = 'M' then 0 when reg.Sexo = 'F' then 0 else 1 end) as 'Unknown',
-SUM(case when (cast((datediff( NOW(), reg.DOB) / 365) AS SIGNED) >= 11) and (cast((datediff( NOW(), reg.DOB) / 365) AS SIGNED) <= 17) then 1 else 0 end) as 'age11to17',
-SUM(case when (cast((datediff( NOW(), reg.DOB) / 365) AS SIGNED) >= 18) and (cast((datediff( NOW(), reg.DOB) / 365) AS SIGNED) <= 24) then 1 else 0 end) as 'age18to24',
-SUM(case when (cast((datediff( NOW(), reg.DOB) / 365) AS SIGNED) > 24)  then 1 else 0 end) as 'morethan24',
-SUM(case when reg.DOB = null then 1 else 0 end) as 'UnknownAge',
-count(reg.Sexo) as 'Total', provider.entity_id as 'provider_id'
+SUM(case when Sexo = 'M' then 1 else 0 end) as 'Male', 
+SUM(case when Sexo = 'F' then 1 else 0 end) as 'Female',
+SUM(case when Sexo = 'M' then 0 when Sexo = 'F' then 0 else 1 end) as 'Unknown',
+SUM(case when (cast((datediff( NOW(), DOB) / 365) AS SIGNED) >= 11) and (cast((datediff( NOW(), DOB) / 365) AS SIGNED) <= 17) then 1 else 0 end) as 'age11to17',
+SUM(case when (cast((datediff( NOW(), DOB) / 365) AS SIGNED) >= 18) and (cast((datediff( NOW(), DOB) / 365) AS SIGNED) <= 24) then 1 else 0 end) as 'age18to24',
+SUM(case when (cast((datediff( NOW(), DOB) / 365) AS SIGNED) > 24)  then 1 else 0 end) as 'morethan24',
+SUM(case when DOB = null then 1 else 0 end) as 'UnknownAge',
+count(uuid) as 'Total', 
+entity_id as 'provider_id',
+field_agency_name_value as provider_name
+from (
+select distinct marp.uuid, sexo, dob, entity_id, field_agency_name_value
 FROM bitnami_drupal7.aj_registration reg
 join bitnami_drupal7.aj_marp marp on marp.uuid=reg.uuid
 join bitnami_drupal7.field_data_field_agency_name provider on provider.entity_id=marp.provider_id
-
-
-
-
 where 1 = 1 
--- don't really need the 1 = 1 but if the other where's go away, it IS needed.
--- 
-
 --IF=:provider_id
-and provider.entity_id = :provider_id
+	and provider.entity_id in (:provider_id)
 --END
-
-
-
 and  marp.Hombresquetienensexoconhombres = 'true'
+) distinctHSH
+group by provider_id
 
 UNION 
--- ACCESS=access content
+
 SELECT 
 'TRSX',
-SUM(case when reg.Sexo = 'M' then 1 else 0 end) as 'Male', 
-SUM(case when reg.Sexo = 'F' then 1 else 0 end) as 'Female',
-SUM(case when reg.Sexo = 'M' then 0 when reg.Sexo = 'F' then 0 else 1 end) as 'Unknown',
-SUM(case when (cast((datediff( NOW(), reg.DOB) / 365) AS SIGNED) >= 11) and (cast((datediff( NOW(), reg.DOB) / 365) AS SIGNED) <= 17) then 1 else 0 end) as 'age11to17',
-SUM(case when (cast((datediff( NOW(), reg.DOB) / 365) AS SIGNED) >= 18) and (cast((datediff( NOW(), reg.DOB) / 365) AS SIGNED) <= 24) then 1 else 0 end) as 'age18to24',
-SUM(case when (cast((datediff( NOW(), reg.DOB) / 365) AS SIGNED) > 24)  then 1 else 0 end) as 'morethan24',
-SUM(case when reg.DOB = null then 1 else 0 end) as 'UnknownAge',
-count(reg.Sexo) as 'Total', provider.entity_id as 'provider_id'
+SUM(case when Sexo = 'M' then 1 else 0 end) as 'Male', 
+SUM(case when Sexo = 'F' then 1 else 0 end) as 'Female',
+SUM(case when Sexo = 'M' then 0 when Sexo = 'F' then 0 else 1 end) as 'Unknown',
+SUM(case when (cast((datediff( NOW(), DOB) / 365) AS SIGNED) >= 11) and (cast((datediff( NOW(), DOB) / 365) AS SIGNED) <= 17) then 1 else 0 end) as 'age11to17',
+SUM(case when (cast((datediff( NOW(), DOB) / 365) AS SIGNED) >= 18) and (cast((datediff( NOW(), DOB) / 365) AS SIGNED) <= 24) then 1 else 0 end) as 'age18to24',
+SUM(case when (cast((datediff( NOW(), DOB) / 365) AS SIGNED) > 24)  then 1 else 0 end) as 'morethan24',
+SUM(case when DOB = null then 1 else 0 end) as 'UnknownAge',
+count(uuid) as 'Total', 
+entity_id as 'provider_id',
+field_agency_name_value as provider_name
+from (
+select distinct marp.uuid, sexo, dob, entity_id, field_agency_name_value
 FROM bitnami_drupal7.aj_registration reg
 join bitnami_drupal7.aj_marp marp on marp.uuid=reg.uuid
 join bitnami_drupal7.field_data_field_agency_name provider on provider.entity_id=marp.provider_id
-
-
-
-
 where 1 = 1 
--- don't really need the 1 = 1 but if the other where's go away, it IS needed.
--- 
-
 --IF=:provider_id
-and provider.entity_id = :provider_id
+	and provider.entity_id in (:provider_id)
 --END
-
-
-
 and  marp.Lostrabajadoresdelsexo = 'true'
+) distinctTRSX
+group by provider_id
 
 UNION
 
 SELECT 
 'PWID',
-SUM(case when reg.Sexo = 'M' then 1 else 0 end) as 'Male', 
-SUM(case when reg.Sexo = 'F' then 1 else 0 end) as 'Female',
-SUM(case when reg.Sexo = 'M' then 0 when reg.Sexo = 'F' then 0 else 1 end) as 'Unknown',
-SUM(case when (cast((datediff( NOW(), reg.DOB) / 365) AS SIGNED) >= 11) and (cast((datediff( NOW(), reg.DOB) / 365) AS SIGNED) <= 17) then 1 else 0 end) as 'age11to17',
-SUM(case when (cast((datediff( NOW(), reg.DOB) / 365) AS SIGNED) >= 18) and (cast((datediff( NOW(), reg.DOB) / 365) AS SIGNED) <= 24) then 1 else 0 end) as 'age18to24',
-SUM(case when (cast((datediff( NOW(), reg.DOB) / 365) AS SIGNED) > 24)  then 1 else 0 end) as 'morethan24',
-SUM(case when reg.DOB = null then 1 else 0 end) as 'UnknownAge',
-count(reg.Sexo) as 'Total', provider.entity_id as 'provider_id'
+SUM(case when Sexo = 'M' then 1 else 0 end) as 'Male', 
+SUM(case when Sexo = 'F' then 1 else 0 end) as 'Female',
+SUM(case when Sexo = 'M' then 0 when Sexo = 'F' then 0 else 1 end) as 'Unknown',
+SUM(case when (cast((datediff( NOW(), DOB) / 365) AS SIGNED) >= 11) and (cast((datediff( NOW(), DOB) / 365) AS SIGNED) <= 17) then 1 else 0 end) as 'age11to17',
+SUM(case when (cast((datediff( NOW(), DOB) / 365) AS SIGNED) >= 18) and (cast((datediff( NOW(), DOB) / 365) AS SIGNED) <= 24) then 1 else 0 end) as 'age18to24',
+SUM(case when (cast((datediff( NOW(), DOB) / 365) AS SIGNED) > 24)  then 1 else 0 end) as 'morethan24',
+SUM(case when DOB = null then 1 else 0 end) as 'UnknownAge',
+count(uuid) as 'Total', 
+entity_id as 'provider_id',
+field_agency_name_value as provider_name
+from (
+select distinct marp.uuid, sexo, dob, entity_id, field_agency_name_value
 FROM bitnami_drupal7.aj_registration reg
 join bitnami_drupal7.aj_marp marp on marp.uuid=reg.uuid
 join bitnami_drupal7.field_data_field_agency_name provider on provider.entity_id=marp.provider_id
-
-
-
-
 where 1 = 1 
--- don't really need the 1 = 1 but if the other where's go away, it IS needed.
--- 
-
 --IF=:provider_id
-and provider.entity_id = :provider_id
+	and provider.entity_id in (:provider_id)
 --END
-
-
-
 and  marp.Usuariosdedrogasintravenosas = 'true'
+) distinctPWID
+group by provider_id
